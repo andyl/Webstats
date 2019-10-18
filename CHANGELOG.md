ae7cb4b7fcb4f75c1713ebb52332043a322ba0d8
andyl <andy@r210.com>
2019-10-18 13:39:09 -0700

    Add CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Badger Changelog
+
+# v0.0.1 [ 2019 Oct 15 ]
+
+Initial Creation
+
--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Badger.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [
       apps_path: "apps",
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -14,10 +16,10 @@ defmodule Badger.MixProject do
 
   defp deps do
     [
-      # ----- deployment
-      {:distillery, "~> 2.1", warn_missing: false},
       # ----- development and test
-      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
+      # ----- utilities
+      {:version_tasks, "~> 0.11.3"}
     ]
   end
 

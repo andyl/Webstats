939044b0fb8380fb533f7ce2a233e3a4bb316ce3
andyl <andy@r210.com>
2020-06-29 05:40:44 -0700

    Refactor plugs

---
+++ apps/badger_web/lib/badger_web/controllers/plug/site_tag.ex
@@ -0,0 +1,23 @@
+defmodule BadgerWeb.SiteTag do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    # usr_id = get_session(conn, :usr_id)
+    # user = usr_id && BadgerData.Api.User.user_get(usr_id)
+    # assign(conn, :current_user, user)
+    conn
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:usr_id, user.id)
+    |> configure_session(renew: true)
+  end
+
+  def logout(conn) do
+    configure_session(conn, drop: true)
+  end
+end
---
+++ apps/badger_web/lib/badger_web/controllers/util/host.ex
@@ -0,0 +1,9 @@
+defmodule BadgerWeb.Util.Host do
+  use Memoize
+
+  defmemo badger_host do
+  end
+
+  defmemo badger_404 do
+  end
+end
